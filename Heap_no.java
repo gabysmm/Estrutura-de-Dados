54e91b8251c29c4e04fbe86deaf2aba032daec76
Gabrielly Magalhães <157931491+gabysmm@users.noreply.github.com>
2025-01-21 18:50:46 +0000

    heap no e atualizações no heap array

--- Heap_no.java
+++ Heap_no.java
@@ -1,28 +1,46 @@
-import java.util.*;
+import java.util.ArrayList;
+
+class Node {
+    int key;
+
+    public Node(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+}
 
 public class Heap {
-    private ArrayList<Integer> heap;
+    private ArrayList<Node> heap;
+    private int size;
 
     public Heap() {
         heap = new ArrayList<>();
+        size = 0;
     }
 
     public boolean isEmpty() {
-        return heap.isEmpty();
+        return size == 0;
     }
 
     public int size() {
-        return heap.size();
+        return size;
     }
 
-    public int min() {
-        if (heap.size() == 0) {
+    public Node min() {
+        if (size == 0) {
             throw new IllegalStateException("Heap vazio");
         }
         return heap.get(0);
     }
 
-    public void insert(int element) {
+    public void insert(Node element) {
         heap.add(element);
         size++;
         upheap(heap.size() - 1);
@@ -31,7 +49,7 @@ public class Heap {
     private void upheap(int i) {
         while (i > 0) {
             int parentI = (i - 1) / 2;
-            if (heap.get(i) >= heap.get(parentI)) {
+            if (heap.get(i).getKey() >= heap.get(parentI).getKey()) {
                 break;
             }
             swap(i, parentI);
@@ -39,30 +57,30 @@ public class Heap {
         }
     }
 
-    public int removeMin() {
-        if (heap.size() == 0) {
+    public Node removeMin() {
+        if (size == 0) {
             throw new IllegalStateException("Heap vazio");
         }
-        int minElement = heap.get(0);
-        heap.set(0, heap.remove(heap.size() - 1));
+        Node minElement = heap.get(0);
+        heap.set(0, heap.remove(size - 1));
         size--;
-        if (heap.size() > 0) {
+        if (size > 0) {
             downheap(0);
         }
         return minElement;
     }
 
     private void downheap(int i) {
-        int lastI = heap.size() - 1;
+        int lastI = size - 1;
         while (i <= lastI) {
             int leftChildI = 2 * i + 1;
             int rightChildI = 2 * i + 2;
             int smallI = i;
 
-            if (leftChildI <= lastI && heap.get(leftChildI) < heap.get(smallI)) {
+            if (leftChildI <= lastI && heap.get(leftChildI).getKey() < heap.get(smallI).getKey()) {
                 smallI = leftChildI;
             }
-            if (rightChildI <= lastI && heap.get(rightChildI) < heap.get(smallI)) {
+            if (rightChildI <= lastI && heap.get(rightChildI).getKey() < heap.get(smallI).getKey()) {
                 smallI = rightChildI;
             }
             if (smallI == i) {
@@ -74,7 +92,7 @@ public class Heap {
     }
 
     private void swap(int a, int b) {
-        int temp = heap.get(a);
+        Node temp = heap.get(a);
         heap.set(a, heap.get(b));
         heap.set(b, temp);
     }
